32236985e12ae9b264c433add29ac4fc6ef6c608
blouk <nicolas@blouk.com>
2017-02-07 16:18:22 +0100

    [ADD]

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Fart Scroll Odoo',
+    'version': '0.1',
+    'summary': 'Fart Scroll Odoo',
+    'category':  'Theme/Environment',
+    'description':
+        """
+        """,
+    'depends': ['web'],
+    'data': [
+        'views/theme.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
