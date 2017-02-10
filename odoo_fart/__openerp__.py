0640da7be2d4a7a7f0b3e03c5aadf867237ac4bd
Wisniewski <nicolas@blouk.com>
2017-02-10 12:05:26 +0100

    add author

--- odoo_fart/__openerp__.py
+++ odoo_fart/__openerp__.py
@@ -2,6 +2,7 @@
     'name': 'Fart Scroll Odoo',
     'version': '0.1',
     'summary': 'Fart Scroll Odoo',
+    'author': 'nicolas@blouk.com',
     'category':  'Theme/Environment',
     'description':
         """
